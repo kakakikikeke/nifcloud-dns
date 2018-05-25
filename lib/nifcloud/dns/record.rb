3cde119423095f4bfe70706fda38894ef0fba446
kakakikikeke <cdl.yoshinaka@gmail.com>
2018-05-25 13:39:25 +0900

    able to set options in list method

--- lib/nifcloud/dns/record.rb
+++ lib/nifcloud/dns/record.rb
@@ -4,14 +4,19 @@ module Nifcloud
   module DNS
     class Record < Client
       RECORD_SET_WITH_ID = 'hostedzone/%s/rrset'
+      RECORD_SET_WITH_PARAMS = 'hostedzone/%s/rrset?%s'
 
       def initialize(zone_id, access_key: nil, secret_key: nil)
         super(access_key, secret_key)
         @zone_id = zone_id
       end
 
-      def list
-        get(RECORD_SET_WITH_ID % @zone_id)
+      def list(options: nil)
+        if options.nil? || !options.instance_of?(Hash)
+          get(RECORD_SET_WITH_ID % @zone_id)
+        else
+          get(RECORD_SET_WITH_PARAMS % [@zone_id, options.collect { |k, v| "#{k}=#{CGI::escape(v.to_s)}" }.join('&')])
+        end
       end
 
       def add(name, type, value, ttl: 86400, comment: '')
